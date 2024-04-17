1d40eede428b98ec611c36a997c89a96a0ce6c24
Erik <erikpersson0884@gmail.com>
2024-04-17 20:45:17 +0200

    added ability to change theme

---
+++ js/theme.js
@@ -0,0 +1,28 @@
+const themeButton = document.getElementById('themeButton');
+const themeButtonImg = document.getElementById('themeButtonImg');
+
+
+let currentTheme;
+
+
+if (localStorage.getItem('theme')){
+    currentTheme = localStorage.getItem('theme');
+    document.body.classList.add(currentTheme);
+}
+
+themeButton.onclick = () => {
+    toggleTheme();
+}
+
+function toggleTheme(){
+    if (currentTheme === 'dark'){        
+        currentTheme = 'light';
+    } else {
+        currentTheme = 'dark';
+    }
+    document.body.classList.remove('dark', 'light');
+    document.body.classList.add(currentTheme);
+    localStorage.setItem('theme', currentTheme);
+
+    themeButtonImg.src = `./img/${currentTheme}.svg`;
+}
